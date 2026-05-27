907a6eddca20e250b8d96a2522806b5ed7c6797a
root <alexanderkapuza2@gmail.com>
2026-05-28 00:54:30 +0300

    initial commit

---
+++ service_input/app_input/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Disease(models.Model):
+    disease = models.CharField(max_length = 50, verbose_name = 'Название болезни')
+    def __str__(self):
+        return self.disease
+
+
+
+
+class Human (models.Model):
+    name = models.CharField(max_length = 100, verbose_name = 'ФИО')
+
+    LIST_GENDER = [
+        ('M', 'Мужской'),
+        ('F', 'Женский'),
+    ]
+
+    gender = models.CharField(max_length = 1, choices = LIST_GENDER, verbose_name = 'Пол')
+    birthday = models.DateField(verbose_name = 'Дата рождения')
+    disease = models.ManyToManyField(Disease, verbose_name = 'Заболевание')
+
+    def __str__(self):
+        return self.name
