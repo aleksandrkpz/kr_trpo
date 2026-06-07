417be061f421779fa07d8508700302ae5ddb9cc1
root <alexanderkapuza2@gmail.com>
2026-06-07 07:33:06 +0300

    add CI/CD pipeline

--- service_input/app_input/models.py
+++ service_input/app_input/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-
+# объявляем класс Болезнь, чтобы иметт возможность использовать свяь Many-to-Many
 class Disease(models.Model):
     disease = models.CharField(max_length = 50, verbose_name = 'Название болезни')
     def __str__(self):
         return self.disease
 
-
-
-
 class Human (models.Model):
     name = models.CharField(max_length = 100, verbose_name = 'ФИО')
-
     LIST_GENDER = [
         ('M', 'Мужской'),
         ('F', 'Женский'),
@@ -18,6 +14,7 @@ class Human (models.Model):
     city = models.CharField(max_length= 100, verbose_name= 'Город',default='Не указан')
     gender = models.CharField(max_length = 1, choices = LIST_GENDER, verbose_name = 'Пол')
     birthday = models.DateField(verbose_name = 'Дата рождения')
+    # связь многие-ко-многим
     disease = models.ManyToManyField(Disease, verbose_name = 'Заболевание')
 
     def __str__(self):
